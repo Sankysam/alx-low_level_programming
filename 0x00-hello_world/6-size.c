4d1ef72d5c618dd3b70b49c047f54e657bcefe26
Sankysam <sankysam05@gmail.com>
2023-06-15 03:37:41 -0700

    many

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (void)
+{
+	printf("size of char: % zu bytes(s)\n", sizeof (char));
+	 printf("size of int: % zu bytes(s)\n", sizeof (int));
+	  printf("size of long int: % zu bytes(s)\n", sizeof (long int));
+	   printf("size of long long int: % zu bytes(s)\n", sizeof (long long int));
+	    printf("size of float: % zu bytes(s)\n", sizeof (float));
+	    return (0);
+
+	
