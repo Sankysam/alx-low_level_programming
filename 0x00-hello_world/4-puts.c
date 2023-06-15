7f943dde3aa860decc8f3d46dbfea31b898c4b47
Sankysam <sankysam05@gmail.com>
2023-06-15 04:54:08 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main -prints "Programming is like building a multilingual puzzle ,
+ * main -prints 
+ *return -always 0
  * followed by a new line
  * return 0.
  */
-int main (void)
+int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
