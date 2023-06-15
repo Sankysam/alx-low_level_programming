8da4abc3b548b145e4d8b7e6d8b6aa193adf2cd8
Sankysam <sankysam05@gmail.com>
2023-06-15 03:46:08 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main -prints "Programming is like building a multilingual puzzle ,
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\n"programming is like a multilingual puzzle");
+	puts("\"programming is like a multilingual puzzle");
 	return (0);
 }
